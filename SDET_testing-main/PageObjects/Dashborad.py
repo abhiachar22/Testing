5a58715ff5fc4b916742cdc5004a0f5931423d16
Abhishek Vishwanath <abhiachar86@gmail.com>
2026-06-15 19:25:04 +0530

    dashboard

--- SDET_testing-main/PageObjects/Dashborad.py
+++ SDET_testing-main/PageObjects/Dashborad.py
@@ -3,8 +3,8 @@
 
 class DashboardPage:
     
-    def __init__(self,page):
-        self.page=page
+    def __init__(self,page):#initializing the page object
+        self.page=page# passing the page object to the class variable
         
     def order_navigation(self):
         self.page.get_by_role("button", name="  ORDERS").click()
